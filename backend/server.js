399f60310862bda73389dcbecceb700770b4b999
Pranoy Basu <23693033+pranoybasu@users.noreply.github.com>
2021-04-02 04:25:29 +0530

    Added ratings

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import path from 'path'
 import express from'express'
 import dotenv from 'dotenv'
 import colors from 'colors'
+import morgan from 'morgan'
 import connectDB from './config/db.js'
 import { notFound, errorHandler } from './middleware/errorMiddleware.js'
 
@@ -17,6 +18,10 @@ connectDB()
 
 //run server
 const app = express()
+
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'))
+}
 
 app.use(express.json())
 
