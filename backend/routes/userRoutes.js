c3462d15b75d9f12ca090d32c03f340332ffffa2
Pranoy Basu <23693033+pranoybasu@users.noreply.github.com>
2021-04-02 01:46:26 +0530

    Added user changeable capabilities to Admin Dashboard

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,6 +1,6 @@
 import express from "express";
 import mongoose from "mongoose";
-import { authUser, registerUser, getUserProfile, updateUserProfile, getUsers } from '../controllers/userController.js'
+import { authUser, registerUser, getUserProfile, updateUserProfile, getUsers, deleteUser, getUserById, updateUser } from '../controllers/userController.js'
 import { protect, admin } from '../middleware/authMiddleware.js';
  
 const router = express.Router();
@@ -10,5 +10,11 @@ router.route('/').post(registerUser).get(protect, admin, getUsers)
 
 router.post('/login', authUser)
 router.route('/profile').get(protect, getUserProfile).put(protect, updateUserProfile)
+
+router
+  .route('/:id')
+  .delete(protect, admin, deleteUser)
+  .get(protect, admin, getUserById)
+  .put(protect, admin, updateUser)
 
 export default router;
